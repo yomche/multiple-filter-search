c770246b83ed6554b70f0701d712c8db756e1b1f
Ekaterina Dorozhkina <katerina.smirnova98@yandex.ru>
2024-03-29 20:39:29 +0300

    refactor

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,13 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/multiple-filter-search/",
   server: {
     host: "127.0.0.1",
     port: 3000,
+  },
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
   },
 });
